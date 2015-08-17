f50aa8e3bfedc1003ae3d78e6dd777cc9b15056c
kdq <375143304@qq.com>
2015-08-17 22:03:05 +0800

    Update Models

--- field_in_hand_server/app/models.py
+++ field_in_hand_server/app/models.py
@@ -29,7 +29,7 @@ class Point(models.Model):
     strike = models.FloatField(verbose_name='走向')
     declination = models.FloatField(verbose_name='磁偏角')
     unitId = models.CharField(max_length=20)
-    timedate = models.DateTimeField(default=now(), editable=True, verbose_name='时间')
+    timedate = models.DateTimeField(default=now, editable=True, verbose_name='时间')
     notes = models.TextField(verbose_name='笔记')
     comments = models.TextField(verbose_name='评论')
     description = models.TextField(verbose_name='描述')
