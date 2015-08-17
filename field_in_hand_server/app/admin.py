256974f7eab4296f42e85f9673fff21584f6b3e9
kdq <375143304@qq.com>
2015-08-17 18:30:06 +0800

    Create Models_v1

--- field_in_hand_server/app/admin.py
+++ field_in_hand_server/app/admin.py
@@ -1,1 +1,1 @@
-from django.contrib import admin# Register your models here.
+from django.contrib import adminfrom models import AppUser, Pointadmin.site.register(Point)admin.site.register(AppUser)# Register your models here.
--- field_in_hand_server/app/models.py
+++ field_in_hand_server/app/models.py
@@ -1,3 +1,46 @@
+# coding=utf-8
 from django.db import models
+import uuid
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
+class Point(models.Model):
+    localityId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    localityName = models.CharField(max_length=100)
+    dataId = models.UUIDField(default=uuid.uuid4, editable=False)
+    datatype = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    x = models.DecimalField(max_digits=20, decimal_places=10)
+    y = models.DecimalField(max_digits=20, decimal_places=10)
+    h = models.DecimalField(max_digits=20, decimal_places=10)
+    latitude = models.DecimalField(max_digits=20, decimal_places=10)
+    longitude = models.DecimalField(max_digits=20, decimal_places=10)
+    altitude = models.DecimalField(max_digits=20, decimal_places=10)
+    horiz_precision = models.DecimalField(max_digits=20, decimal_places=10)
+    vert_precision = models.DecimalField(max_digits=20, decimal_places=10)
+    distanceNS = models.DecimalField(max_digits=20, decimal_places=10)
+    distanceEW = models.DecimalField(max_digits=20, decimal_places=10)
+    planeType = models.CharField(max_length=20)
+    dip = models.FloatField()
+    dipAzumuth = models.FloatField()
+    speed = models.FloatField()
+    plunge = models.CharField(max_length=20)
+    plungeAzimuth = models.CharField(max_length=20)
+    strike = models.FloatField()
+    declination = models.FloatField()
+    unitId = models.CharField(max_length=20)
+    timedate = models.DateTimeField(default=now(), editable=True)
+    notes = models.TextField()
+    comments = models.TextField()
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.localityId
+
+class AppUser(User):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    usertype = models.CharField(max_length=20)
+    databaseSize = models.IntegerField()
+    language = models.CharField(max_length=20)
+    vectorgraphNumber = models.IntegerField()
 # Create your models here.
