d3f6cb5cd64fede59a23675fbd969c7edb1ec160
kdq <375143304@qq.com>
2015-09-09 19:21:48 +0800

    Add DataSerializer

--- app/serializer.py
+++ app/serializer.py
@@ -1,5 +1,5 @@
 __author__ = 'kdq'
-from app.models import AppUser
+from app.models import AppUser, Data, Locality, Unit
 from rest_framework import serializers
 
 class AppUserSerializer(serializers.ModelSerializer):
@@ -14,3 +14,12 @@ class AppUserSerializer(serializers.ModelSerializer):
 
     def get_password(self, obj):
         return obj.user.password
+
+
+class DataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Data
+        fields = ('dataId', 'datatype', 'name', 'x', 'y', 'h', 'latitude', 'longitude', 'altitude', 'horiz_precision',
+                  'vert_precision', 'distanceNS', 'distanceEW', 'planeType', 'dip', 'dipAzimuth', 'speed',
+                  'plunge', 'plungeAzimuth', 'strike', 'declination', 'timedate', 'notes', 'comments', 'description',
+                  'heading', 'image_name', 'locality', 'unit')
